de08571761b80b90d5a83a74cf9e5bbe17e79dcc
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-03-18 10:54:37 -0300

    Cria arquivo de conexão com o banco de dados

---
+++ ScreenSound/Banco/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace ScreenSound.Banco
+{
+    internal class Connection
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public SqlConnection ObterConexao()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
