b5507f77865b77eaaad51f49aa45246f3f520bf0
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-03-18 11:03:59 -0300

    Realiza a abertura da conexão com o db

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,5 +1,16 @@
-﻿using ScreenSound.Menus;
+﻿using ScreenSound.Banco;
+using ScreenSound.Menus;
 using ScreenSound.Modelos;
+
+try
+{
+    using var connection = new Connection().ObterConexao();
+    connection.Open();
+} 
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 Artista ira = new Artista("Ira!", "Banda Ira!");
 Artista beatles = new("The Beatles", "Banda The Beatles");
