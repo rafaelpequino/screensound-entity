578064b18234e8bee83f1efd701950c1594cc8aa
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-03-19 10:01:02 -0300

    Cria método de Adicionar Artista

---
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco
+{
+    internal class ArtistaDAL
+    {
+        public List<Artista> Listar()
+        {
+            var lista = new List<Artista>();
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = "SELECT * FROM Artistas";
+            SqlCommand command = new SqlCommand(sql, connection);
+            using SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                string nomeArtista = Convert.ToString(dataReader["Nome"]);
+                string bioArtista = Convert.ToString(dataReader["Bio"]);
+                int idArtista = Convert.ToInt32(dataReader["Id"]);
+                Artista artista = new(nomeArtista, bioArtista) { Id = idArtista };
+
+                lista.Add(artista);
+            }
+
+            return lista;
+        }
+
+        public void Adicionar(Artista artista)
+        {
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = "INSERT INTO Artistas (Nome, FotoPerfil, Bio) VALUES (@nome, @perfilPadrao, @bio)";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@nome", artista.Nome);
+            command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
+            command.Parameters.AddWithValue("@bio", artista.Bio);
+
+            int retorno = command.ExecuteNonQuery();
+            if (retorno > 0)
+            {
+                Console.WriteLine("Artista inserido com sucesso");
+            }
+        }
+    }
+}
