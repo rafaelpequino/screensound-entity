ba4b0cccedf89295ec9e9ce1b41f483b4c058dc6
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-03-20 09:53:03 -0300

    Adiciona método RecuperarPeloNome

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -35,5 +35,10 @@ namespace ScreenSound.Banco
             context.Artistas.Remove(artista);
             context.SaveChanges();
         }
+
+        public Artista? RecuperarPeloNome(string nome)
+        {
+            return context.Artistas.FirstOrDefault(a => a.Nome.Equals(nome));
+        }
     }
 }
